9ce555ac526343194a628b5223b726338c0d775e
seszcze <sebastian@ably.pl>
2018-04-26 22:03:51 +0200

    work in progress

---
+++ src/CommandsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaPress\Commands;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommandsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
